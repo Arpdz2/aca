7f7ff7cd2966ef2dde47e966e21f182a8f6ef1a8
arpdz2 <arpdz2@umsl.edu>
2015-10-26 19:16:38 -0500

    fixes

--- routes/sendEmail.js
+++ routes/sendEmail.js
@@ -10,7 +10,7 @@ exports.passwordReset = function(req) {
     employee.findOne({ 'email' : req.body.email }, function(err, user) {
             if (err) {
                 console.log(err);
-            } else if (user) {
+            } else if (user && user != null) {
                 
                 var password = Math.random().toString(36).slice(-8);
                 var mongo = new employee();
@@ -30,7 +30,7 @@ exports.passwordReset = function(req) {
                 
                 transport.sendMail({
                     from: 'ACA Insurance Group  <noreply@acainsuresme.com>',
-                    to: 'brenden.mckamey@gmail.com',
+                    to: user.email,
                     subject: 'ACA Insurance Credentials',
                     html: '<p>Dear ' + user.firstname + ',<br/>Your temporary password for ACA Insurance is below. Your username will be sent in a separate email. Please use the link below to login.<br/><b>Password: </b>' + password + '<br/><b>Link to site: </b>' + req.protocol + '://' + req.get("host") + '<br/>If you have any questions or issues regarding access to ACA Insurance, please e-mail EMAILHERE or call TEAMHERE at NUMBERHERE.</p><p>Thank you,<br/>ACA Insurance Group</p>'
                 }, function(err, info) {
@@ -43,7 +43,7 @@ exports.passwordReset = function(req) {
 
                 transport.sendMail({
                     from: 'ACA Insurance Group  <noreply@acainsuresme.com>',
-                    to: 'brenden.mckamey@gmail.com',
+                    to: user.email,
                     subject: 'ACA Insurance Credentials',
                     html: '<p>Dear ' + user.firstname + ',<br/>Your username for ACA Insurance is below. Your temporary password will be sent in a separate email. Please use the link below to login.<br/><b>Username: </b>' + user.email + '<br/><b>Link to site: </b>' + req.protocol + '://' + req.get("host") + '<br/>If you have any questions or issues regarding access to ACA Insurance, please e-mail EMAILHERE or call TEAMHERE at NUMBERHERE.</p><p>Thank you,<br/>ACA Insurance Group</p>'
                 }, function(err, info) {
@@ -54,5 +54,6 @@ exports.passwordReset = function(req) {
                     }
                 });
             }
+
         });    
 };
