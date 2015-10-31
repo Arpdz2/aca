2321c0335f320ceea5c469c1acf4f8005406964f
arpdz2 <arpdz2@umsl.edu>
2015-10-31 18:56:41 -0500

    session update

--- public/js/checkloginsession.js
+++ public/js/checkloginsession.js
@@ -1,16 +1,22 @@
 function checkLoginSession() {
-    if(document.cookie == "login=True") {
-        document.getElementById("status").className ="btn btn-danger";
-        document.getElementById("status").innerHTML = "Logout";
-        document.getElementById("status").href="/logout";
-        document.getElementById("status").onclick= removeLoginSession;
-        console.log(document.cookie);
-    }
-    else {
-        document.getElementById("status").className ="btn btn-success";
-        document.getElementById("status").innerHTML = "Login";
-        document.getElementById("status").href="/login";
-        console.log(document.cookie);
+    var x = document.cookie.split(";");
+    for (var i = 0; i < x.length ; i++) {
+        str = x[i].replace(/\s+/g, '');
+        console.log(str);
+        if(str == "login=True") {
+            document.getElementById("status").className ="btn btn-danger";
+            document.getElementById("status").innerHTML = "Logout";
+            document.getElementById("status").href="/logout";
+            document.getElementById("status").onclick= removeLoginSession;
+            console.log(document.cookie);
+            break;
+        }
+        else {
+            document.getElementById("status").className ="btn btn-success";
+            document.getElementById("status").innerHTML = "Login";
+            document.getElementById("status").href="/login";
+            console.log(document.cookie);
+        }
     }
 }
 
