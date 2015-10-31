1da4f2a0ac8d08bf7ffaee61f54b42c38671fa6f
arpdz2 <arpdz2@umsl.edu>
2015-10-31 18:42:28 -0500

    session update

--- public/js/checkloginsession.js
+++ public/js/checkloginsession.js
@@ -1,13 +1,13 @@
 function checkLoginSession() {
     if(document.cookie == "login=True") {
-        document.getElementById("status").class="btn btn-danger";
+        document.getElementById("status").className ="btn btn-danger";
         document.getElementById("status").innerHTML = "Logout";
         document.getElementById("status").href="/logout";
         document.getElementById("status").onclick= removeLoginSession;
         console.log(document.cookie);
     }
     else {
-        document.getElementById("status").class="btn btn-danger";
+        document.getElementById("status").className ="btn btn-success";
         document.getElementById("status").innerHTML = "Login";
         document.getElementById("status").href="/login";
         console.log(document.cookie);
