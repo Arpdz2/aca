1c83cfb6b19628aeef6c6aba5ebb249bf0a63a4e
arpdz2 <arpdz2@umsl.edu>
2015-10-31 18:26:56 -0500

    session fix

--- public/js/forcehttps.js
+++ public/js/forcehttps.js
@@ -1,9 +1,11 @@
 function forceHttps() {
     var string = window.location.toString();
     if (window.location.protocol == 'https:' || string.indexOf("localhost") != -1) {
+        checkLoginSession()
     }
     else {
         window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+        checkLoginSession()
     }
 }
 window.onload = forceHttps;
