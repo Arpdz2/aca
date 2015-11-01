215963a4f0398cf505d24e5700d827ebef5b7962
arpdz2 <arpdz2@umsl.edu>
2015-11-01 13:40:08 -0600

    fixed sessions for DST

--- public/js/setloginsession.js
+++ public/js/setloginsession.js
@@ -1,7 +1,7 @@
 function setLoginSession() {
     var d = new Date();
     //set 15 minute sessions
-    d.setMinutes(d.getMinutes() + 315);
+    d.setMinutes(d.getMinutes() + 375);
     document.cookie="login=True; expires=" + d.toString() + ";";
     checkLoginSession();
 }
